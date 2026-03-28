28a5daf32e17302c2fdf8ef0dfbb538ec09a6d1d
sergi-e <ishserge16@gmail.com>
2026-03-28 23:31:19 +0200

    Debugged the next function

--- ISBN_validator.py
+++ ISBN_validator.py
@@ -25,25 +25,21 @@ def validate_isbn(isbn, length):
         print('Valid ISBN Code.')
     else:
         print('Invalid ISBN Code.')
+        
 def calculate_check_digit_10(main_digits_list):
-    # Note: You don't have to fully understand the logic in this function.
     digits_sum = 0
-    # Multiply each of the first 9 digits by its corresponding weight (10 to 2) and sum up the results
     for index, digit in enumerate(main_digits_list):
         digits_sum += digit * (10 - index)
-    # Find the remainder of dividing the sum by 11, then subtract it from 11
+
     result = 11 - digits_sum % 11
-    # The calculation result can range from 1 to 11.
-    # If the result is 11, use 0.
-    # If the result is 10, use upper case X.
-    # Use the value as it is for other numbers.
+
     if result == 11:
-        expected_check_digit = '0'
+        return '0'
     elif result == 10:
-        expected_check_digit = 'X'
+        return 'X'
     else:
-        expected_check_digit = str(result)
-    return expected_check_digit
+        return str(result)
+    
 def calculate_check_digit_13(main_digits_list):
     # Note: You don't have to fully understand the logic in this function.
     digits_sum = 0
